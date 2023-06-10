4f3c76afa79393d5d7bf32240500ffbf2cc76856
maddala-venky <venky.mv01@gmail.com>
2023-06-10 12:36:19 +0530

    javascript and html practise

---
+++ test.js
@@ -0,0 +1,555 @@
+// var a = 7;
+// var b = 78;
+// c = a + b;
+// console.log(c);
+
+// var x = 9;
+// var x = 8;
+// Z = x;
+// console.log(Z);
+
+// // var x = 12;
+// let w = 13;
+// let y = 11;
+// console.log(w - y);
+
+// const o = 56;
+// // const o = 34;
+
+// console.log(o);
+
+// let A = 12;
+// // let A = 23;
+// let B = 24;
+
+// console.log(A + B);
+
+// var T = 23;
+// var Q = "11";
+
+// console.log(typeof T);
+// console.log(typeof Q);
+// console.log(T + Q);
+
+// Q > T ? console.log("Q is big") : console.log("T is big");
+
+// let result;
+// result = String(123);
+// console.log(result);
+
+// {
+//   var x = 2;
+
+//   console.log(x);
+// }
+
+//function myFunction() {
+//let carName = "volvo";
+//console.log(carName);
+//}
+
+//myFunction();
+
+// let user = "venky";
+//console.log(user);
+
+// user = "v\ten\vk\ny";
+//console.log(user);
+
+//let num = Number("234");
+//console.log(num, typeof num);
+
+//let user = String("venky");
+//console.log(user, typeof user);
+
+// let x = "venky";
+// let y = "samapath";
+// let data = x > y;
+// console.log(data);
+
+// let x = "venky";
+// let y = "hero";
+// let data = x < y;
+// console.log(data);
+
+// let x = 3,
+//   y = 5,
+//   z = 9;
+// let result = x < y || y > z;
+// console.log(result);
+
+// let num1 = 8;
+// let num2 = 9;
+// let result = num1 > num2;{
+
+// if (result) console.log("num1 is greater");
+
+// console.log("venky");
+// }
+// selse {
+//    console.log("num2 is greater");
+// console.log("hi..");
+// }
+
+// let num1 = 3;
+// let num2 = 5;
+// let num3 = 6;
+// if (num1 > num2 && num1 > nu3) {
+//   console.log("num1 is smallest");
+// } else if (num2 > num3) {
+//   console.log("num2 is smallest");
+// } else {
+//   console.log("num3 is greatest");
+// }
+// console.log("venky");
+
+//mon - 7Am
+//tue - thurs - 4Am
+//fri -9Am
+//sat - sun -8Am
+
+// let day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("7Am");
+//   case "Tuesday":
+//     console.log("4Am");
+
+//   case "Wedensday":
+//     console.log("4Am");
+//   case "Thursday":
+//     console.log("4Am");
+//   case "Friday":
+//     console.log("9Am");
+//   case "Saturday":
+//     console.log("8Am");
+//   case "Sunday":
+//     console.log("7Am");
+// }
+
+// let num1 = 4;
+// let num2 = 5;
+// result = num1 - num2;
+// console.log("The subtraction of" + num1 + "and" + num2 + "is" + result);
+// console.log("my name is \nvenky");
+// console.log("my hero is \vprabhas");
+
+//let i = 1;
+
+// while (i <= 9) {
+//   console.log("venky");
+//   i++;
+// }
+
+// let i = 4;
+// while (i <= 7) {
+//   console.log("hi");
+//   i++;
+// }
+
+// for (let i = 1; i < 5; i++) {
+//   console.log("hi", i);
+// }
+// let i;
+
+// for (i = 1; i < 9; i++) {
+//   console.log("hai", i);
+// }
+// i++;
+
+//let i;
+//for (i = 2; i < 12; i = i * 2) {
+//console.log("hai", i);
+//}
+
+// for (i = 0; i < 14; i++)
+//   if (i % 2 === 0) {
+//     console.log("hai", i);
+//   }
+
+// for (i = 0; i < 12; i++)
+//   if ((i % 2) + 1) {
+//     console.log("hai", i);
+//   }
+
+// for (i = 0; i < 50; i = i + 1) {
+//   if (i % 5 == 0 && i % 7 == 0) {
+//     console.log("hi", i);
+//   }
+// }
+
+// let alien = {
+//   father: "suryanarayana",
+//   mother: "lakshmi",
+//   me: "venky",
+//   brother: "sampath",
+//   sister: "madhuri",
+// };
+// console.log(alien);
+
+// let add = function (num1, num2) {
+//   return num1 + num2;
+// };
+
+// let sum = add;
+// let result = sum(5, 6);
+// console.log(result);
+
+// function greet(u) {
+//   console.log(user);
+//   return `Hello ${u}!!`;
+// }
+// let user = "venky";
+// let str = greet(user);
+// console.log(str);
+
+//
+
+// let family = {
+//   father: "suryanarayana",
+//   mother: "lakshmi",
+//   brother: "sampath",
+//   sister: "madhuri",
+
+//   getConfig: function () {
+//     console.log(this.brother);
+//   },
+// };
+
+// family.getConfig();
+
+//
+
+// {
+/* <html>
+  <body>
+    <h2> my javascript </h2>
+
+    <button
+      type="button"
+      onClick="document.getElementById('demo').innerHTML = Date()"
+    >
+      click me to display Date and Time.
+    </button>
+    <p id="demo"></p>
+  </body>
+</html>; */
+// }
+
+// function myFunction() {
+//   let carName = "volvo";
+//   console.log(carName);
+// }
+// myFunction();
+
+//
+// var marks = 100;
+// if (marks > 90) {
+//   console.log("A grade");
+// } else if (marks > 70) {
+//   console.log("B grade");
+// } else if (marks > 60) {
+//   console.log("C grade");
+// } else if (marks > 50) {
+//   console.log("D grade");
+// } else if (marks > 40) {
+//   console.log("E grade");
+// } else {
+//   console.log("Fail");
+// }
+// hoistedVariable = 9;//initialization
+// console.log(hoistedVariable); // outputs 3 even when the variable is declared after it is initialized
+// var hoistedVariable;//declaration
+
+// var x;
+// x = 10;
+// console.log(x)
+// Outputs " Hello world! " even when the function is declared after calling
+
+// (function Function() {
+//   debugger;
+//   console.log(" Hello world! ");
+// })();
+// // Function();
+
+// (function () {
+//   console.log("hknskjn");
+// })();
+
+// const ids = [1, 2, 3, 4, 5, 6];
+// const listElements = ids.map((id) => {
+//   return <li key={ids.tostring()}>{id}</li>;
+// });
+
+// var obj1 = {
+//   address: "Mumbai,India",
+//   getAddress: function () {
+//     console.log(this.address);
+//   },
+// };
+
+// var getAddress = obj1.getAddress;
+// var obj2 = { name: "akshay" };
+// obj2.getAddress();
+
+// function venky() {
+//   var a = 5;
+//   console.log(a);
+// }
+// venky();
+
+// function sam() {
+//   fullName: {
+//     (firstName = "maddala"), (lastName = "sampath"), console.log(fullName);
+//   }
+// }
+// sam();
+
+// x = "venky";
+// console.log(x);
+
+// let num1 = 5;
+// let num2 = 9;
+// let result = num1 < num2;
+// if (result) console.log("num2 is greater");
+// console.log("hi");
+
+// for (i = 0; i < 50; i = i + 1) {
+//   if (i % 2 == 0 && i % 6 == 0) {
+//     console.log("hi", i);
+//   }
+// }
+
+// var name = "venky";
+// name = "maddala";
+// var name = "rohith";
+// console.log(name);
+
+// let name = "venky";
+// name = "maddala";
+// console.log(name);
+
+// const name = "venky";
+// console.log(name);
+
+// function venky() {
+//   var name = "venky";
+//   var age = "22";
+//   console.log(name, age);
+// }
+// venky();
+
+// function intro(name, age) {
+//   var extName = `MR ${name}`;
+//   var newAge = age + 2;
+//   return " hello, name is " + extName + " his age is " + newAge + " years old";
+// }
+// console.log(intro("venky", 22));
+
+//var names = ["venky", "sampath", "madhuri"];
+//console.log(names[1]);
+//console.log(names);
+
+// var list = ["one", "two", "three", "four", "five", "six"];
+// console.log(list);
+// console.log(list[1]);
+// console.log(list[2]);
+// console.log(list[3]);
+// console.log(list[4]);
+// console.log(list.length);
+// console.log(list.indexOf("three"));
+// console.log(list[6]);
+// console.log(list.push("four"));
+// console.log(list);
+// console.log(list[5]);
+// console.log(list.length);
+// console.log(list.shift());
+// console.log(list.unshift());
+// console.log(list.push());
+
+//let name = ["latha", "sri"];
+//console.log(name.join(" "));
+
+//let name = "venky sampath";
+//console.log(name.split(" "));
+//console.log(name.split(" "));
+
+// var user = {
+//   name: "venky",
+//   age: 22,
+//   gender: "Male",
+// };
+// console.log(user.name);
+
+// var data = {
+//   products: [
+//     { title: "apple mac", price: 500 },
+//     { title: "boAt watch", price: 59 },
+//     { title: "oneplus Tv", price: 400 },
+//     { title: "Fridge", price: 1200 },
+//   ],
+
+//   userName: "venky",
+//   city: "vizag",
+//   country: "india",
+// };
+
+// console.log(data.products["i"]);
+// for (var i = 0; i < 4; i++) {
+// console.log(data.products[i].title);
+//}
+
+//console.log(data.products[0]);
+//console.log(data.products[1]);
+//console.log(data.products[2]);
+//console.log(data.products[3]);
+
+//console.log(data.products[(0, 1, 2, 3)]);
+
+//for (var i = 0; i < 10; i++) {
+//console.log(i);
+//}
+
+//
+
+// let user = "venky";
+// console.log(user);
+
+// const person = {
+//   name: "venky",
+//   age: "22",
+//   tech: "developer",
+// };
+// console.log(person.age);
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// let text = "";
+
+// list: {
+//   text += cars[0] + "<br>";
+//   text += cars[1] + "<br>";
+//   break list;
+//   text += cars[2] + "<br>";
+//   text += cars[3] + "<br>";
+// }
+
+// console.log(cars[0], cars[1]);
+
+// let hello = "";
+
+// hello = function () {
+//   return "hello world!";
+// };
+
+//console.log(hi);
+// console.log(hello());
+
+// const v = 4;
+// console.log(v);
+
+// var obj = {
+//   name: "venky",
+//   display: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// var obj1 = {
+//   name: "ABC",
+// };
+
+//object.display()
+//
+
+// let map = new Map();
+
+// map.set("Venky", "Ece");
+// map.set("Madhuri", "Ece");
+// map.set("Samapth", "Cse");
+
+// console.log(map.get("Sampath"));
+
+// const array = [3, 3, 6, 9, 8];
+
+// max;
+
+// function max(arr) {
+//   let max = 0;
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// console.log(max(array));
+
+// const users = [
+//   { name: "venky", tech: "soft", age: "23" },
+//   { name: "madhuri", tech: "soft", age: "21" },
+//   { name: "sampath", tech: "soft", age: "20" },
+//];
+// venky
+
+//const output = users.filter((x) => x.age < 21);
+//const output = users.filter((x) => x.age > 21);
+//const output = users.filter((x) => x.age < 21).map((x) => x.firstName);
+
+//const name = users.map((users) => users.name.toUpperCase());
+//const details = users.filter((users) => users.newAge());
+
+// console.log(details);
+
+// console.log(start);
+
+// setTimeout(function cbT() {
+//   console.log("CB setTimeout");
+// }, 5000);
+
+// fetch("https://api.netflix.com").then(function cbF() {
+//   console.log("CB Netflix");
+// });
+// console.log("end");
+
+// for (Leti = 1; i < 100; i++) {
+//   if (i % 2 == 0);
+
+//   console.log(i);
+// }
+// let num = 546782;
+// while (num > 0) {
+//   console.log(num % 10);
+//   num = parseInt(num / 10);
+//   console.log(num);
+// }
+
+// function greet(u) {
+//   console.log(u);
+//   return `Hello ${u}!!`;
+// }
+
+// let greet = function () {
+//   console.log("Helloworld");
+//   return 1;
+// };
+// console.log(greet());
+
+// return 67;
+
+// let laptop = {
+//   cpu: "23",
+//   ram: 12,
+//   brand: "Acer",
+// };
+// console.log(laptop.brand);
+
+//for (let i = 1; i < 100; i++) console.log(i);
+
+function numbers() {
+  for (let i = 1; i < 100; i++) {
+    console.log(i);
+  }
+  numbers();
+}
